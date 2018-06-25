11af556835ed9ff7f2b4e474f9a886388ed2f067
k3lv1nd <kalvind95@gmail.com>
2018-06-25 11:21:31 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.write("app one");
+document.write("app one");
+document.write("app one");
+document.write("helllo eneki you are so stunning");
+document.write("helllo thomas you are so stunning");
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: ["./app.js", "./app2.js"],
+  output: {
+    filename: "main.js"
+  }
+}
